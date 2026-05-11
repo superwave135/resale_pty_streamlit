b4f461c3824fc52ce8770b77bcf4b9c53aecd4af
superwave135 <superwave135@yahoo.com.sg>
2026-05-11 11:38:55 +0800

    Update dashboard name

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ df = load_data(DATA_PATH)
 # You can set the page title and layout here
 st.set_page_config(page_title="HDB Resale Dashboard", layout="wide")
 
-st.title("🏠 Singapore HDB Resale Dashboard")
+st.title("🏠 HDB Resale Analytics Dashboard ")
 # st.caption("Code-along: building a usable dashboard from real resale transactions.")
 
 # st.header("Dashboard Overview")
